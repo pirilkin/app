7cd4e45a5f67a7beb364c9b2454d7b740766de77
pirilkin <63636989+pirilkin@users.noreply.github.com>
2020-04-30 17:54:40 +0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    
+    $('.about-slider').slick({
+        slidesToShow: 4,
+        autoplay: true,
+        prevArrow: '<button class="about-slider__prev"><i class="fa fa-angle-left"></i></button>',
+        nextArrow: '<button class="about-slider__next"><i class="fa fa-angle-right"></i></button>',
+        
+    });
+    
+});
